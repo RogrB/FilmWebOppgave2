4c599ceb151caf98515ae88c64d32e02775bd9c7
Laptop\Roger <roger.birkenes+github@gmail.com>
2018-10-24 18:10:52 +0200

    Laget enhetstester for rediger nyhet

--- Enhetstest/DBTester/RedigerNyhetTest.cs
+++ Enhetstest/DBTester/RedigerNyhetTest.cs
@@ -14,11 +14,11 @@ using System.Web.Mvc;
 namespace Enhetstest.DBTester
 {
     [TestClass]
-    public class RedigerBrukerTest
+    public class RedigerNyhetTest
     {
-        
+
         [TestMethod]
-        public void RedigerBrukereViewOK()
+        public void RedigerNyheterViewOK()
         {
             // Arrange
             var controller = new AdminController(new AdminLogikk(new AdminRepositoryStub()));
@@ -26,33 +26,22 @@ namespace Enhetstest.DBTester
             SessionMock.InitializeController(controller);
             controller.Session["Admin"] = "admin";
 
-            var forventetResultat = new List<Kunde>();
-            var kunde = new Kunde()
+            List<Nyhet> forventetResultat = new List<Nyhet>();
+            Nyhet enNyhet = new Nyhet()
             {
                 id = 1,
-                Fornavn = "Ole",
-                Etternavn = "Olesen",
-                Kort = 1234567890987654,
-                Brukernavn = "ole0",
-                Filmer = new List<Film>()
+                Tittel = "Nyhet01",
+                Dato = "Dato01",
+                Beskjed = "Dette er en nyhet"
             };
-            var film = new Film()
-            {
-                id = 1,
-                Navn = "Film01",
-                Bilde = "Bilde01",
-                Beskrivelse = "Beskrivelse01",
-                Kontinent = "USA"
-            };
-            kunde.Filmer.Add(film);
 
-            forventetResultat.Add(kunde);
-            forventetResultat.Add(kunde);
-            forventetResultat.Add(kunde);
+            forventetResultat.Add(enNyhet);
+            forventetResultat.Add(enNyhet);
+            forventetResultat.Add(enNyhet);
 
             // Act
-            var actionResult = (ViewResult)controller.RedigerBrukere();
-            var resultat = (List<Kunde>)actionResult.Model;
+            var actionResult = (ViewResult)controller.RedigerNyheter();
+            var resultat = (List<Nyhet>)actionResult.Model;
 
             // Assert
             Assert.AreEqual(actionResult.ViewName, "");
@@ -60,18 +49,14 @@ namespace Enhetstest.DBTester
             for (var i = 0; i < resultat.Count; i++)
             {
                 Assert.AreEqual(forventetResultat[i].id, resultat[i].id);
-                Assert.AreEqual(forventetResultat[i].Fornavn, resultat[i].Fornavn);
-                Assert.AreEqual(forventetResultat[i].Etternavn, resultat[i].Etternavn);
-                Assert.AreEqual(forventetResultat[i].Kort, resultat[i].Kort);
-                Assert.AreEqual(forventetResultat[i].Brukernavn, resultat[i].Brukernavn);
-                Assert.AreEqual(forventetResultat[i].Filmer[0].id, resultat[i].Filmer[0].id);
-                Assert.AreEqual(forventetResultat[i].Filmer[0].Navn, resultat[i].Filmer[0].Navn);
-                Assert.AreEqual(forventetResultat[i].Filmer[0].Bilde, resultat[i].Filmer[0].Bilde);
+                Assert.AreEqual(forventetResultat[i].Tittel, resultat[i].Tittel);
+                Assert.AreEqual(forventetResultat[i].Dato, resultat[i].Dato);
+                Assert.AreEqual(forventetResultat[i].Beskjed, resultat[i].Beskjed);
             }
         }
 
         [TestMethod]
-        public void RedigerBrukereViewNullSession()
+        public void RedigerNyheterViewNullSession()
         {
             // Arrange
             var controller = new AdminController(new AdminLogikk(new AdminRepositoryStub()));
@@ -80,7 +65,7 @@ namespace Enhetstest.DBTester
             controller.Session["Admin"] = null;
 
             // Act
-            var actionResult = (RedirectToRouteResult)controller.RedigerBrukere();
+            var actionResult = (RedirectToRouteResult)controller.RedigerNyheter();
 
             // Assert
             Assert.AreEqual(actionResult.RouteName, "");
@@ -88,7 +73,7 @@ namespace Enhetstest.DBTester
         }
 
         [TestMethod]
-        public void RedigerBrukereViewBlankSession()
+        public void RedigerNyheterViewBlankSession()
         {
             // Arrange
             var controller = new AdminController(new AdminLogikk(new AdminRepositoryStub()));
@@ -97,7 +82,7 @@ namespace Enhetstest.DBTester
             controller.Session["Admin"] = "";
 
             // Act
-            var actionResult = (RedirectToRouteResult)controller.RedigerBrukere();
+            var actionResult = (RedirectToRouteResult)controller.RedigerNyheter();
 
             // Assert
             Assert.AreEqual(actionResult.RouteName, "");
@@ -105,52 +90,38 @@ namespace Enhetstest.DBTester
         }
 
         [TestMethod]
-        public void RedigerBrukerViewOK()
+        public void RedigerNyhetViewOK()
         {
             // Arrange
             var controller = new AdminController(new AdminLogikk(new AdminRepositoryStub()));
             var SessionMock = new TestControllerBuilder();
             SessionMock.InitializeController(controller);
             controller.Session["Admin"] = "admin";
-            var forventetResultat = new EndreKunde()
+
+            Nyhet forventetResultat = new Nyhet()
             {
                 id = 1,
-                Fornavn = "Ole",
-                Etternavn = "Olesen",
-                Kort = 1234567890987654,
-                Brukernavn = "ole0",
-                Filmer = new List<Film>()
+                Tittel = "Nyhet01",
+                Dato = "Dato01",
+                Beskjed = "Dette er en nyhet"
             };
-            var film = new Film()
-            {
-                id = 1,
-                Navn = "Film01",
-                Bilde = "Bilde01",
-                Beskrivelse = "Beskrivelse01",
-                Kontinent = "USA"
-            };
-            forventetResultat.Filmer.Add(film);
 
 
             // Act
-            var actionResult = (ViewResult)controller.RedigerBruker(forventetResultat.id);
-            var resultat = (EndreKunde)actionResult.Model;
+            var actionResult = (ViewResult)controller.RedigerNyhet(forventetResultat.id);
+            var resultat = (Nyhet)actionResult.Model;
 
             // Assert
             Assert.AreEqual(actionResult.ViewName, "");
 
             Assert.AreEqual(forventetResultat.id, resultat.id);
-            Assert.AreEqual(forventetResultat.Fornavn, resultat.Fornavn);
-            Assert.AreEqual(forventetResultat.Etternavn, resultat.Etternavn);
-            Assert.AreEqual(forventetResultat.Kort, resultat.Kort);
-            Assert.AreEqual(forventetResultat.Brukernavn, resultat.Brukernavn);
-            Assert.AreEqual(forventetResultat.Filmer[0].id, resultat.Filmer[0].id);
-            Assert.AreEqual(forventetResultat.Filmer[0].Navn, resultat.Filmer[0].Navn);
-            Assert.AreEqual(forventetResultat.Filmer[0].Bilde, resultat.Filmer[0].Bilde);
+            Assert.AreEqual(forventetResultat.Tittel, resultat.Tittel);
+            Assert.AreEqual(forventetResultat.Dato, resultat.Dato);
+            Assert.AreEqual(forventetResultat.Beskjed, resultat.Beskjed);
         }
 
         [TestMethod]
-        public void RedigerBrukerViewNullModel()
+        public void RedigerNyhetViewNullModel()
         {
             // Arrange
             var controller = new AdminController(new AdminLogikk(new AdminRepositoryStub()));
@@ -159,16 +130,16 @@ namespace Enhetstest.DBTester
             controller.Session["Admin"] = "admin";
 
             // Act
-            var actionResult = (ViewResult)controller.RedigerBruker(0);
-            var KundeResultat = (EndreKunde)actionResult.Model;
+            var actionResult = (ViewResult)controller.RedigerNyhet(0);
+            var NyhetsResultat = (Nyhet)actionResult.Model;
 
             // Assert
             Assert.AreEqual(actionResult.ViewName, "");
-            Assert.AreEqual(KundeResultat.id, 0);
+            Assert.AreEqual(NyhetsResultat.id, 0);
         }
 
         [TestMethod]
-        public void RedigerBrukerViewNullSession()
+        public void RedigerNyhetViewNullSession()
         {
             // Arrange
             var controller = new AdminController(new AdminLogikk(new AdminRepositoryStub()));
@@ -177,7 +148,7 @@ namespace Enhetstest.DBTester
             controller.Session["Admin"] = null;
 
             // Act
-            var actionResult = (RedirectToRouteResult)controller.RedigerBruker();
+            var actionResult = (RedirectToRouteResult)controller.RedigerNyhet();
 
             // Assert
             Assert.AreEqual(actionResult.RouteName, "");
@@ -185,7 +156,7 @@ namespace Enhetstest.DBTester
         }
 
         [TestMethod]
-        public void RedigerBrukerViewBlankSession()
+        public void RedigerNyhetViewBlankSession()
         {
             // Arrange
             var controller = new AdminController(new AdminLogikk(new AdminRepositoryStub()));
@@ -194,7 +165,7 @@ namespace Enhetstest.DBTester
             controller.Session["Admin"] = "";
 
             // Act
-            var actionResult = (RedirectToRouteResult)controller.RedigerBruker();
+            var actionResult = (RedirectToRouteResult)controller.RedigerNyhet();
 
             // Assert
             Assert.AreEqual(actionResult.RouteName, "");
@@ -202,34 +173,25 @@ namespace Enhetstest.DBTester
         }
 
         [TestMethod]
-        public void RedigerBrukerIkkeFunnetIPost()
+        public void RedigerNyhetIkkeFunnetIPost()
         {
             // Arrange
             var controller = new AdminController(new AdminLogikk(new AdminRepositoryStub()));
             var SessionMock = new TestControllerBuilder();
             SessionMock.InitializeController(controller);
             controller.Session["Admin"] = "admin";
-            var innKunde = new EndreKunde()
+
+            Nyhet innNyhet = new Nyhet()
             {
                 id = 0,
-                Fornavn = "Ole",
-                Etternavn = "Olesen",
-                Kort = 1234567890987654,
-                Brukernavn = "ole0",
-                Filmer = new List<Film>()
+                Tittel = "Nyhet01",
+                Dato = "Dato01",
+                Beskjed = "Dette er en nyhet"
             };
-            var film = new Film()
-            {
-                id = 1,
-                Navn = "Film01",
-                Bilde = "Bilde01",
-                Beskrivelse = "Beskrivelse01",
-                Kontinent = "USA"
-            };
-            innKunde.Filmer.Add(film);
+
 
             // Act
-            var actionResult = (ViewResult)controller.RedigerBruker(innKunde);
+            var actionResult = (ViewResult)controller.RedigerNyhet(innNyhet);
 
             // Assert
             Assert.AreEqual(actionResult.ViewName, "");
@@ -237,35 +199,26 @@ namespace Enhetstest.DBTester
         }
 
         [TestMethod]
-        public void RedigerBrukerFeilValideringIPost()
+        public void RedigerNyhetFeilValideringIPost()
         {
             // Arrange
             var controller = new AdminController(new AdminLogikk(new AdminRepositoryStub()));
             var SessionMock = new TestControllerBuilder();
             SessionMock.InitializeController(controller);
             controller.Session["Admin"] = "admin";
-            var innKunde = new EndreKunde()
+
+            Nyhet innNyhet = new Nyhet()
             {
                 id = 1,
-                Fornavn = "Ole",
-                Etternavn = "Olesen",
-                Kort = 1234567890987654,
-                Brukernavn = "ole0",
-                Filmer = new List<Film>()
+                Tittel = "Nyhet01",
+                Dato = "Dato01",
+                Beskjed = "Dette er en nyhet"
             };
-            var film = new Film()
-            {
-                id = 1,
-                Navn = "Film01",
-                Bilde = "Bilde01",
-                Beskrivelse = "Beskrivelse01",
-                Kontinent = "USA"
-            };
-            innKunde.Filmer.Add(film);
+
             controller.ViewData.ModelState.AddModelError("feil", "ID = 0");
 
             // Act
-            var actionResult = (ViewResult)controller.RedigerBruker(innKunde);
+            var actionResult = (ViewResult)controller.RedigerNyhet(innNyhet);
 
             // Assert
             Assert.IsTrue(actionResult.ViewData.ModelState.Count == 1);
@@ -274,34 +227,24 @@ namespace Enhetstest.DBTester
         }
 
         [TestMethod]
-        public void RedigerBrukerPostOK()
+        public void RedigerNyhetPostOK()
         {
             // Arrange
             var controller = new AdminController(new AdminLogikk(new AdminRepositoryStub()));
             var SessionMock = new TestControllerBuilder();
             SessionMock.InitializeController(controller);
             controller.Session["Admin"] = "admin";
-            var innKunde = new EndreKunde()
+
+            Nyhet innNyhet = new Nyhet()
             {
                 id = 1,
-                Fornavn = "Ole",
-                Etternavn = "Olesen",
-                Kort = 1234567890987654,
-                Brukernavn = "ole0",
-                Filmer = new List<Film>()
+                Tittel = "Nyhet01",
+                Dato = "Dato01",
+                Beskjed = "Dette er en nyhet"
             };
-            var film = new Film()
-            {
-                id = 1,
-                Navn = "Film01",
-                Bilde = "Bilde01",
-                Beskrivelse = "Beskrivelse01",
-                Kontinent = "USA"
-            };
-            innKunde.Filmer.Add(film);
 
             // Act
-            var actionResult = (ViewResult)controller.RedigerBruker(innKunde);
+            var actionResult = (ViewResult)controller.RedigerNyhet(innNyhet);
 
             // Assert
             Assert.AreEqual(actionResult.ViewName, "");
