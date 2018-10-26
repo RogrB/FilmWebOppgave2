33d019706878788ebc735ac367a619566b515244
Laptop\Roger <roger.birkenes+github@gmail.com>
2018-10-26 23:14:31 +0200

    regEx funker.. bedre

--- Model/Nyhet.cs
+++ Model/Nyhet.cs
@@ -11,11 +11,11 @@ namespace Model
         public int id { get; set; }
 
         [Required(ErrorMessage = "Tittel må oppgis")]
-        [RegularExpression(@"^[A-Za-z0-9æøåØÆÅ@_\- ]{1,}", ErrorMessage = "Tittel kan ikke inneholde spesialtegn")]
+        [RegularExpression(@"^[A-Za-z0-9æøåØÆÅ@_\- !,.]{1,}", ErrorMessage = "Tittel kan ikke inneholde spesialtegn")]
         public string Tittel { get; set; }
 
         [Required(ErrorMessage = "Beskjed må oppgis")]
-        [RegularExpression(@"^[A-Za-z0-9æøåØÆÅ@_\- ]{1,}", ErrorMessage = "Beskjed kan ikke inneholde spesialtegn")]
+        [RegularExpression(@"^[A-Za-z0-9æøåØÆÅ@_\- !,.]{1,}", ErrorMessage = "Beskjed kan ikke inneholde spesialtegn")]
         public string Beskjed { get; set; }
 
         public string Dato { get; set; }
