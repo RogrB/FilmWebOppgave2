11eee770b16de5a6dba6a562c36762cb621c2944
RogrB <36276260+RogrB@users.noreply.github.com>
2018-10-31 21:35:17 +0100

    Add files via upload

---
+++ Graubakken Filmsjappe/Controllers/AdminController.cs
@@ -0,0 +1,491 @@
+﻿using BLL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace Graubakken_Filmsjappe.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IAdminLogikk _adminBLL;
+
+        public AdminController()
+        {
+            _adminBLL = new AdminLogikk();
+        }
+
+        public AdminController(IAdminLogikk stub)
+        {
+            _adminBLL = stub;
+        }
+
+        public ActionResult Index()
+        {
+            if (AdminLoggetInn())
+            {
+                return RedirectToAction("Admin");
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+        public ActionResult Admin()
+        {
+            if (AdminLoggetInn())
+            {
+                return View();
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+        public bool AdminLoggetInn()
+        {
+            return Session["Admin"] != null && (string)Session["Admin"] != "";
+        }
+
+        public ActionResult AdminLoginn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AdminLoginn(Administrator innAdmin)
+        {
+            if (_adminBLL.SjekkInnLogging(innAdmin))
+            {
+                Session["Admin"] = innAdmin.Brukernavn;
+                return RedirectToAction("Admin");
+            }
+            else
+            {
+                Session["Admin"] = "";
+                ViewBag.AdminLogin = "Feil brukernavn eller passord";
+                return View();
+            }
+        }
+
+        public ActionResult RegistrerAdmin()
+        {
+            var resultat = _adminBLL.RegistrerAdmin();
+            if (resultat == "OK")
+            {
+                ViewBag.AdminResultat = "Admin opprettet";
+            }
+            else if(resultat == "Admin er allerede registrert")
+            {
+                ViewBag.AdminResultat = "Admin er allerede registrert";
+            }
+            else
+            {
+                ViewBag.AdminResultat = "Kunne ikke registrere admin";
+            }
+            return View();
+        }
+
+        public ActionResult Logut()
+        {
+            Session["Admin"] = "";
+
+            return RedirectToAction("AdminLoginn");
+        }
+
+        public ActionResult RedigerFilmer()
+        {
+            if(AdminLoggetInn())
+            {
+                List<Film> alleFilmer = _adminBLL.HentFilmer();
+                return View(alleFilmer);
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+        public ActionResult RedigerFilm(int id = 0)
+        {
+            if(AdminLoggetInn())
+            {
+                Film film = _adminBLL.HentFilm(id);
+                return View(film);
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RedigerFilm(Film innFilm, HttpPostedFileBase bildeOpplasting)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_adminBLL.RedigerFilm(innFilm, bildeOpplasting))
+                {
+                    ViewBag.EndreStatus = "Informasjon oppdatert";
+                }
+                else
+                {
+                    ViewBag.EndreStatus = "Klarte ikke å oppdatere informasjon";
+                }
+            }
+            return View(_adminBLL.HentFilm(innFilm.id));
+        }
+
+        public ActionResult RedigerSkuespillere()
+        {
+            if(AdminLoggetInn())
+            {
+                var skuespillere = _adminBLL.HentSkuespillere();
+                return View(skuespillere);
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+        public ActionResult RedigerSkuespiller(int id = 0)
+        {
+            if (AdminLoggetInn())
+            {
+                var skuespiller = _adminBLL.HentSkuespiller(id);
+                return View(skuespiller);
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RedigerSkuespiller(Skuespiller innSkuespiller, HttpPostedFileBase bildeOpplasting)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_adminBLL.RedigerSkuespiller(innSkuespiller, bildeOpplasting))
+                {
+                    ViewBag.EndreStatus = "Informasjon oppdatert";
+                }
+                else
+                {
+                    ViewBag.EndreStatus = "Klarte ikke å oppdatere informasjon";
+                }
+            }
+            return View(_adminBLL.HentSkuespiller(innSkuespiller.id));
+        }
+
+        public ActionResult RedigerBrukere()
+        {
+            if(AdminLoggetInn())
+            {
+                var brukere = _adminBLL.HentKunder();
+                return View(brukere);
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+        public ActionResult RedigerBruker(int id = 0)
+        {
+            if(AdminLoggetInn())
+            {
+                var kunde = _adminBLL.HentKunde(id);
+                return View(kunde);
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RedigerBruker(EndreKunde innKunde)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_adminBLL.RedigerKunde(innKunde))
+                {
+                    ViewBag.EndreStatus = "Informasjon oppdatert";
+                }
+                else
+                {
+                    ViewBag.EndreStatus = "Klarte ikke å oppdatere informasjon";
+                }
+            }
+            return View(_adminBLL.HentKunde(innKunde.id));
+        }
+
+        public ActionResult OpprettSkuespiller()
+        {
+            if(AdminLoggetInn())
+            {
+                return View();
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult OpprettSkuespiller(Skuespiller innSkuespiller, HttpPostedFileBase bildeOpplasting)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_adminBLL.OpprettSkuespiller(innSkuespiller, bildeOpplasting))
+                {
+                    return RedirectToAction("RedigerSkuespillere");
+                }
+            }
+            ViewBag.OpprettStatus = "Klarte ikke å opprette skuespiller";
+            return View();
+        }
+
+        public ActionResult OpprettFilm()
+        {
+            if (AdminLoggetInn())
+            {
+                return View();
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult OpprettFilm(Film innFilm, HttpPostedFileBase bildeOpplasting)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_adminBLL.OpprettFilm(innFilm, bildeOpplasting))
+                {
+                    return RedirectToAction("RedigerFilmer");
+                }
+            }
+            ViewBag.OpprettStatus = "Klarte ikke å opprette film";
+            return View();
+        }
+
+        public ActionResult RedigerNyheter()
+        {
+            if(AdminLoggetInn())
+            {
+                var nyheter = _adminBLL.HentNyheter();
+                return View(nyheter);
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+        public ActionResult RedigerNyhet(int id = 0)
+        {
+            if (AdminLoggetInn())
+            {
+                var kunde = _adminBLL.HentNyhet(id);
+                return View(kunde);
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RedigerNyhet(Nyhet innNyhet)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_adminBLL.RedigerNyhet(innNyhet))
+                {
+                    ViewBag.EndreStatus = "Informasjon oppdatert";
+                }
+                else
+                {
+                    ViewBag.EndreStatus = "Klarte ikke å oppdatere informasjon";
+                }
+            }
+            return View(_adminBLL.HentNyhet(innNyhet.id));
+        }
+
+        public ActionResult OpprettNyhet()
+        {
+            if(AdminLoggetInn())
+            {
+                return View();
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult OpprettNyhet(Nyhet innNyhet)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_adminBLL.OpprettNyhet(innNyhet))
+                {
+                    return RedirectToAction("RedigerNyheter");
+                }
+            }
+            ViewBag.OpprettStatus = "Klarte ikke å opprette nyhet";
+            return View();
+        }
+
+        public ActionResult VisLogg()
+        {
+            if (AdminLoggetInn())
+            {
+                string[] logg = _adminBLL.HentLogg();
+                return View(logg);
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+
+
+        // AJAX 
+
+        public string HentSkuespillere()
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var skuespillere = _adminBLL.HentSkuespillere();
+
+            return jsonSerializer.Serialize(skuespillere);
+        }
+
+        public string LeggSkuespillerIFilm(int filmID, int skuespillerID)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var resultat = _adminBLL.LeggSkuespillerIFilm(filmID, skuespillerID);
+
+            return jsonSerializer.Serialize(resultat);
+        }
+
+        public string SlettSkuespillerFraFilm(int filmID, int skuespillerID)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var resultat = _adminBLL.SlettSkuespillerFraFilm(filmID, skuespillerID);
+
+            return jsonSerializer.Serialize(resultat);
+        }
+
+        public string HentSjangereForFilm(int id)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var resultat = _adminBLL.HentSjangereForFilm(id);
+
+            return jsonSerializer.Serialize(resultat);
+        }
+
+        public string HentSjangere()
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var sjangere = _adminBLL.HentSjangere();
+
+            return jsonSerializer.Serialize(sjangere);
+        }
+
+        public string LeggSjangerIFilm(int filmID, int sjangerID)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var resultat = _adminBLL.LeggSjangerIFilm(filmID, sjangerID);
+
+            return jsonSerializer.Serialize(resultat);
+        }
+
+        public string SlettSjangerFraFilm(int filmID, int sjangerID)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var resultat = _adminBLL.SlettSjangerFraFilm(filmID, sjangerID);
+
+            return jsonSerializer.Serialize(resultat);
+        }
+
+        public string HentFilmerForAjax()
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var filmer = _adminBLL.HentFilmerForAjax();
+
+            return jsonSerializer.Serialize(filmer);
+        }
+
+        public string LeggFilmISkuespiller(int skuespillerID, int filmID)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var resultat = _adminBLL.LeggFilmISkuespiller(skuespillerID, filmID);
+
+            return jsonSerializer.Serialize(resultat);
+        }
+
+        public string SlettFilmFraSkuespiller(int skuespillerID, int filmID)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var resultat = _adminBLL.SlettFilmFraSkuespiller(skuespillerID, filmID);
+
+            return jsonSerializer.Serialize(resultat);
+        }
+
+        public string SlettFilmFraBruker(int brukerID, int filmID)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var resultat = _adminBLL.SlettFilmFraBruker(brukerID, filmID);
+
+            return jsonSerializer.Serialize(resultat);
+        }
+
+        public string SlettSkuespiller(int id)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var resultat = _adminBLL.SlettSkuespiller(id);
+            string output = "";
+            if (resultat)
+            {
+                output = "OK";
+            }
+            else
+            {
+                output = "Feil";
+            }
+
+            return jsonSerializer.Serialize(output);
+        }
+
+        public string SlettFilm(int id)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var resultat = _adminBLL.SlettFilm(id);
+            string output = "";
+            if (resultat)
+            {
+                output = "OK";
+            }
+            else
+            {
+                output = "Feil";
+            }
+
+            return jsonSerializer.Serialize(output);
+        }
+
+        public string SlettBruker(int id)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var resultat = _adminBLL.SlettBruker(id);
+            string output = "";
+            if (resultat)
+            {
+                output = "OK";
+            }
+            else
+            {
+                output = "Feil";
+            }
+
+            return jsonSerializer.Serialize(output);
+        }
+
+        public string SlettNyhet(int id)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var resultat = _adminBLL.SlettNyhet(id);
+            string output = "";
+            if (resultat)
+            {
+                output = "OK";
+            }
+            else
+            {
+                output = "Feil";
+            }
+
+            return jsonSerializer.Serialize(output);
+        }
+
+    }
+}
---
+++ Graubakken Filmsjappe/Controllers/FilmController.cs
@@ -0,0 +1,423 @@
+﻿using BLL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace Graubakken_Filmsjappe.Controllers
+{
+    public class FilmController : Controller
+    {
+        private IKundeLogikk _kundeBLL;
+
+        public FilmController()
+        {
+            _kundeBLL = new KundeLogikk();
+        }
+
+        public FilmController(IKundeLogikk stub)
+        {
+            _kundeBLL = stub;
+        }
+
+        /// Metoder for views
+        public ActionResult Index()
+        {
+            IndexView indexView = _kundeBLL.HentIndexView();
+
+            return View(indexView);
+        }
+
+        public ActionResult Skuespillere(string sortering)
+        {
+            if (string.IsNullOrEmpty(sortering))
+            {
+                sortering = "Fornavn";
+                ViewBag.SkuespillerSortering = "Fornavn";
+            }
+            else
+            {
+                ViewBag.SkuespillerSortering = sortering;
+            }
+            List<Skuespiller> alleSkuespillere = _kundeBLL.HentSkuespillerView(sortering);
+            return View(alleSkuespillere);
+        }
+
+        public ActionResult Nyheter()
+        {
+            List<Nyhet> alleNyheter = _kundeBLL.HentAlleNyheter();
+            return View(alleNyheter);
+        }
+
+        public ActionResult Filmer(string sortering)
+        {
+            if (string.IsNullOrEmpty(sortering))
+            {
+                sortering = "Alfabetisk";
+                ViewBag.Sortering = "Alfabetisk";
+            }
+            else
+            {
+                ViewBag.Sortering = sortering;
+            }
+            List<Film> alleFilmer = _kundeBLL.HentFilmView(sortering);
+            return View(alleFilmer);
+        }
+
+        public ActionResult Sjangere()
+        {
+            List<Sjanger> alleSjangere = _kundeBLL.HentAlleSjangere();
+            return View(alleSjangere);
+        }
+
+        public ActionResult Registrer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registrer(Kunde innKunde)
+        {
+            if(!ModelState.IsValid)
+            {
+                ViewBag.RegistreringsStatus = "Skjemaet er ikke fylt inn riktig";
+                return View();
+            }
+            if (!_kundeBLL.SjekkOmBrukernavnErLedig(innKunde.Brukernavn))
+            {
+                ViewBag.RegistreringsStatus = "Brukernavnet er opptatt";
+                return View();
+            }
+            if(_kundeBLL.RegistrerBruker(innKunde))
+            {
+                Session["LoggetInn"] = true;
+                Session["Brukernavn"] = innKunde.Brukernavn;
+                ViewBag.Innlogget = true;
+                return RedirectToAction("Velkommen");
+            }
+            else
+            {
+                ViewBag.RegistreringsStatus = "Feil under registrering";
+                return View();
+            }
+        }
+
+        public ActionResult Velkommen()
+        {
+            if (Session["LoggetInn"] != null)
+            {
+                if ((bool)Session["LoggetInn"])
+                {
+                    ViewBag.Brukernavn = Session["Brukernavn"];
+                    return View();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Loginn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Loginn(Kunde innKunde)
+        {
+            if(_kundeBLL.SjekkInnLogging(innKunde))
+            {
+                Session["LoggetInn"] = true;
+                Session["Brukernavn"] = innKunde.Brukernavn;
+                ViewBag.Brukernavn = innKunde.Brukernavn;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                Session["LoggetInn"] = false;
+                Session["Brukernavn"] = "";
+                ViewBag.Brukernavn = "";
+                return View();
+            }
+        }
+
+        public ActionResult Logut()
+        {
+            Session["LoggetInn"] = false;
+            Session["Brukernavn"] = "";
+            ViewBag.Brukernavn = "";
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Bruker(string brukernavn)
+        {
+            if (Session["LoggetInn"] != null)
+            {
+                if ((bool)Session["LoggetInn"])
+                {
+                    var bruker = _kundeBLL.HentBruker(brukernavn);
+
+                    return View(bruker);
+                }
+            }
+            return RedirectToAction("Loginn");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Bruker(EndreKunde innKunde)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_kundeBLL.EndreBruker(innKunde, (string)Session["Brukernavn"]))
+                {
+                    ViewBag.EndreStatus = "Informasjon oppdatert";
+                }
+                else
+                {
+                    ViewBag.EndreStatus = "Klarte ikke å oppdatere informasjon";
+                }
+            }
+            return View(_kundeBLL.HentBruker((string)Session["Brukernavn"]));
+        }
+
+        public ActionResult Film(int id = 0)
+        {
+            var filmInfo = _kundeBLL.HentFilmInfo(id);
+
+            return View(filmInfo);
+        }
+
+        public ActionResult Skuespiller(int id = 0)
+        {
+            var skuespillerInfo = _kundeBLL.HentSkuespillerInfo(id);
+
+            return View(skuespillerInfo);
+        }
+
+        public ActionResult FilmVisning(int id = 0)
+        {
+            if (Session["LoggetInn"] != null && Session["Brukernavn"] != null)
+            {
+                if ((bool)Session["LoggetInn"])
+                {
+                    if (!_kundeBLL.OppdaterFilmVisningData(id) || !_kundeBLL.LeggFilmIKundeObjekt((string)Session["Brukernavn"], id))
+                    {
+                        ViewBag.VisningsInfo = "En databasefeil har oppstått";
+                    }
+                    var film = _kundeBLL.HentFilmInfo(id);
+                    return View(film);
+                }
+            }
+            return RedirectToAction("Loginn");
+        }
+
+        /// Metoder for ajax-kall
+        public string HentEnFilm(int id)
+        {
+            Film enFilm = _kundeBLL.HentFilm(id);
+            var utFilm = new Film()
+            {
+                id = enFilm.id,
+                Navn = enFilm.Navn,
+                Beskrivelse = enFilm.Beskrivelse,
+                Bilde = enFilm.Bilde,
+                Produksjonsår = enFilm.Produksjonsår,
+                Kontinent = enFilm.Kontinent,
+                ReleaseDate = enFilm.ReleaseDate,
+                Studio = enFilm.Studio,
+                Visninger = enFilm.Visninger,
+                Sjanger = new List<Sjanger>(),
+                Stemmer = new List<Stemme>(),
+                Skuespillere = new List<Skuespiller>()
+            };
+            
+            var jsonSerializer = new JavaScriptSerializer();
+            string jsonData = jsonSerializer.Serialize(utFilm);
+
+            return jsonData;
+        }
+
+        // Lar en bruker gi en "terningkast" stemme på en angitt film
+        public string StemPåFilm(int FilmID, int stemme)
+        {
+            string resultat = "";
+            var jsonSerializer = new JavaScriptSerializer();
+            if (Session["Brukernavn"] != null && (string)Session["Brukernavn"] != "")
+            {
+                if (_kundeBLL.StemPåFilm(FilmID, (string)Session["Brukernavn"], stemme))
+                {
+                    resultat = "OK";
+                }
+                else
+                {
+                    resultat = "Feil";
+                }
+            }
+            else
+            {
+                resultat = "Feil innlogging";
+            }
+            return jsonSerializer.Serialize(resultat);
+        }
+
+        // Henter alle filmer en angitt skuespiller har vært med i
+        public string HentFilmerFraSkuespillerID(int id)
+        {
+            List<Film> filmer = _kundeBLL.HentFilmerFraSkuespillerID(id);
+
+            var jsonSerializer = new JavaScriptSerializer();
+            string jsonData = jsonSerializer.Serialize(filmer);
+
+            return jsonData;
+        }
+
+        // Henter alle skuespillere i en angitt film
+        public string HentSkuespillereIFilm(int id)
+        {
+            List<Skuespiller> skuespillere = _kundeBLL.HentSkuespillereIFilm(id);
+
+            var jsonSerializer = new JavaScriptSerializer();
+            string jsonData = jsonSerializer.Serialize(skuespillere);
+
+            return jsonData;
+        }
+
+        public string SjekkOmBrukernavnErLedig(string brukernavn)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            string resultat = "";
+            if (_kundeBLL.SjekkOmBrukernavnErLedig(brukernavn))
+            {
+                resultat = "Brukernavn Ledig";
+            }
+            else
+            {
+                resultat = "Brukernavn opptatt";
+            }
+            return jsonSerializer.Serialize(resultat);
+        }
+
+        public string LeggIØnskeliste(int FilmID)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            string resultat = "";
+            if (Session["Brukernavn"] != null && (string)Session["Brukernavn"] != "")
+            {
+
+                if (_kundeBLL.LeggIØnskeliste(FilmID, (string)Session["Brukernavn"]))
+                {
+                    resultat = "OK";
+                }
+                else
+                {
+                    resultat = "Feil";
+                }
+            }
+            else
+            {
+                resultat = "Feil innlogging";
+            }
+            return jsonSerializer.Serialize(resultat);
+        }
+
+        public string SlettFraØnskeliste(int id)
+        {
+            string resultat = "";
+            var jsonSerializer = new JavaScriptSerializer();
+            if (_kundeBLL.SlettFraØnskeliste(id, (string)Session["Brukernavn"]))
+            {
+                resultat = "OK";
+            }
+            else
+            {
+                resultat = "Feil";
+            }
+            return jsonSerializer.Serialize(resultat);
+        }
+
+        // Henter en liste av filmer og skuespillere som søkeforslag når en bruker skriver inn i søkefeltet
+        public string HentSøkeforslag(string streng)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            var søkeforslag = _kundeBLL.HentSøkeforslag(streng);
+
+            return jsonSerializer.Serialize(søkeforslag);
+        }
+
+        // Lar en bruker skrive en kommentar på en angitt film
+        public string SkrivKommentar(int id, string Melding)
+        {
+            string resultat = "";
+            var jsonSerializer = new JavaScriptSerializer();
+            if (Session["Brukernavn"] != null && (string)Session["Brukernavn"] != "")
+            {
+                if (_kundeBLL.SkrivKommentar(id, Melding, (string)Session["Brukernavn"]))
+                {
+                    resultat = "OK";
+                }
+                else
+                {
+                    resultat = "Feil";
+                }
+            }
+            else
+            {
+                resultat = "Feil innlogging";
+            }
+            return jsonSerializer.Serialize(resultat);
+        }
+
+        // Henter 3 filmer fra en tilfeldig sjanger som en kunde har sett tidligere
+        public string ForeslåFilm()
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+            if (Session["Brukernavn"] != null && (string)Session["Brukernavn"] != "")
+            {
+                var Filmer = _kundeBLL.ForeslåFilm((string)Session["Brukernavn"]);
+                if (Filmer != null)
+                {
+                    return jsonSerializer.Serialize(Filmer);
+                }
+                else
+                {
+                    return jsonSerializer.Serialize("Feil");
+                }
+            }
+            else
+            {
+                return jsonSerializer.Serialize("Feil innlogging");
+            }
+        }
+
+
+
+        /// Metoder som ikke er del av løsningen, men har blitt brukt under utvikling
+
+        // Metode som mater data inn i DB fra DBData mappen - brukes som erstatning for DBInit
+        public ActionResult Dbinsert()
+        {
+            if (_kundeBLL.InsertDBData())
+            {
+                ViewBag.Message = "DB insert OK";
+            }
+            else
+            {
+                ViewBag.Message = "DB insert feilet";
+            }
+            return View();
+        }
+
+        // Viser Kundeviewet - alle registrerte brukere
+        public ActionResult Kunder()
+        {
+            var kunder = _kundeBLL.HentKunder();
+            return View(kunder);
+        }
+
+    }
+}
